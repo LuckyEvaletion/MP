463da8f75c09d2278273c1b5f9ab304c487c3a99
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-08-06 14:49:40 +0200

    Delete Codes directory

--- Codes/ObjectHighlighter/Implementations/Implementations.lua
+++
@@ -1,4 +0,0 @@
-return {
-	worldColor = loadstring(game:HttpGet("https://github.com/mstudio45/MSDOORS/blob/main/Codes/ObjectHighlighter/Implementations/worldColor.lua?raw=true"))(),
-	highlightColor = loadstring(game:HttpGet("https://github.com/mstudio45/MSDOORS/blob/main/Codes/ObjectHighlighter/Implementations/highlightColor.lua?raw=true"))(),
-}
